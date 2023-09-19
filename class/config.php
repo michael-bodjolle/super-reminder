2233ddcf24235bf45cccd5496a62fb2d0b5c00d2
Michael <michael.bodjolle@laplateforme.io>
2023-09-19 16:22:36 +0200

    bddconect

---
+++ class/config.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+require_once "class/bdd-connect.php";
+$bdd= new Database();
+
+// require_once "class/user.php";
+// $user= new User($bdd);
---
+++ class/connect-bdd.php
@@ -0,0 +1,51 @@
+<?php 
+
+ class Database
+ {
+    private $db_host;
+    private $db_login;
+    private $db_password;
+    private $db_name;
+    public $PDO;
+ 
+ 
+    public function __construct()
+    {
+        $this->db_host = "localhost";
+        $this->db_login = "root";
+        $this->db_password = "";
+        $this->db_name = "super-reminder";
+ 
+ 
+    }
+ 
+ 
+    public function connectDb()
+    {
+        try {
+                //connexion à la base de données
+            $this->PDO = new PDO("mysql:host=$this->db_host;dbname=$this->db_name;charset=utf8", $this->db_login, $this->db_password);
+            //force PDO à préparer les requêtes
+            $this->PDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+            //indique à PDO de bien générer une erreur fatale si un problème survient. 
+            $this->PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           //  var_dump($this->PDO);
+            return $this->PDO;
+ 
+        } catch (PDOException $e) {
+            echo "Erreur : " . $e->getMessage();
+            
+         }
+     }
+     
+ 
+ 
+   public function close(){
+     $this->PDO = null;
+   }
+   
+ 
+ }
+ 
+?>
+
