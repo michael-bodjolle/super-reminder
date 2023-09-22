7867e3e7f2c313b655dc0c699be9fcaca5ed818d
aymen-benaferi <aymen.benaferi@laplateforme.io>
2023-09-22 10:33:20 +0200

    nav

--- header.php
+++ header.php
@@ -17,11 +17,26 @@ if (isset($_SESSION['id'])) {
 
 ?>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Accueil</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="register.php">Inscription</a>
+        <a class="nav-link active" aria-current="page" href="connexion.php">Connexion</a>
+        <a class="nav-link active" aria-current="page" href="">To do list</a>
+      </div>
+    </div>
+  </div>
+</nav>
 <div class="nav">
-<ul>
+<!-- <ul>
   <li><a href="index.php">Accueil</a></li>
   <li><a href="connexion.php">Connexion</a></li>
   <li><a href="register.php">Inscription</a></li>
   <!-- <li><a href="about.asp">About</a></li> -->
-</ul>
-</div>  
+<!-- </ul> --> 
+<!-- </div>   -->
